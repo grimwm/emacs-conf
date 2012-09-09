b02c264e1c033872b48b637ecd9b70b358b53b79
William Grim <william@grimapps.com>
2012-09-09 17:17:29 -0400

    Changing the indentation level from 4 to 2.

--- python-exec.el
+++ python-exec.el
@@ -2,4 +2,5 @@
   ((save-buffer)
    (call-process "python" (buffer-file-name))))
 ;(global-set-key (kbd "M-g p") 'eval-python-on-current-buffer)
+(setq-default python-indent 2)
 (provide 'python-exec)
