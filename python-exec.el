03201654b602c5a01568b50db3c7f088909b42c6
William Grim <william@grimapps.com>
2012-09-23 15:44:57 -0400

    Fixed python-mode indentation level.

--- python-exec.el
+++ python-exec.el
@@ -1,7 +1,15 @@
 (defun eval-python-on-current-buffer ()
   ((save-buffer)
    (call-process "python" (buffer-file-name))))
-;(global-set-key (kbd "M-g p") 'eval-python-on-current-buffer)
-(setq-default py-indent-offset 2)
-(setq-default python-indent 2)
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (setq python-indent 2)
+	    (setq py-indent-offset 2)
+	    (setq python-continuation-offset 2)
+	    (local-set-key (kbd "M-g p") 'eval-python-on-current-buffer)))
+;; (setq-default py-indent-offset 2)
+;; (setq-default python-indent 2)
+;; (setq-default python-continuation-offset 2)
+
 (provide 'python-exec)
